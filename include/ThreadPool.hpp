b98a16e7594ba3ad824acce96717356581ea28ff
UCMHCteacher <100867395+UCMHCteacher@users.noreply.github.com>
2025-02-20 21:38:22 +0800

    feat: Added ThreadPool

---
+++ include/ThreadPool.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <memory>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
+
+    // 禁止复制和赋值
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+
+    // 提交任务并返回一个future对象（支持非void返回类型）
+    template <typename F, typename... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+
+    // 提交任务（支持void返回类型）
+    template <typename F, typename... Args>
+    void enqueue_void(F&& f, Args&&... args);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    bool stop = false;
+};
+
+template <typename F, typename... Args>
+void ThreadPool::enqueue_void(F&& f, Args&&... args) {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        if (stop) {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+        tasks.emplace(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    }
+    condition.notify_one();
+}
+
+
+template <typename F, typename... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        if (stop) {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+        tasks.emplace([task]() { (*task)(); });
+    }
+    condition.notify_one();
+    return res;
+}
+
