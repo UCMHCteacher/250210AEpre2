d065fdaee45de9e9cce2709be932e4bc6a80254a
UCMHCteacher <100867395+UCMHCteacher@users.noreply.github.com>
2025-03-09 13:54:58 +0800

    build: returned to VSCode discovered that we can use VSC+MSVC

---
+++ Configs.cmake
@@ -0,0 +1,7 @@
+set(MySQL_INCLUDE_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 8.0\\include")
+set(MySQL_LIB_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 8.0\\lib64\\vs14")
+
+
+
+set(Npcap_INCLUDE_DIR "C:\\Program Files\\Npcap\\npcap-sdk-1.13\\Include")
+set(Npcap_LIB_DIR "C:\\Program Files\\Npcap\\npcap-sdk-1.13\\Lib\\x64")
