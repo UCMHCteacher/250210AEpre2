9e11a84791ae85eb682c788e25f1048503eecfc3
UCMHCteacher <100867395+UCMHCteacher@users.noreply.github.com>
2025-03-15 12:08:52 +0800

    dependencies: Changed to MySQL Connector/C++ 9.2 because it uses openssl 3.0

--- Configs.cmake
+++ Configs.cmake
@@ -1,7 +1,10 @@
-set(MySQL_INCLUDE_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 8.0\\include")
-set(MySQL_LIB_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 8.0\\lib64\\vs14")
+set(MySQL_INCLUDE_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 9.2\\include")
+set(MySQL_LIB_DIR "C:\\Program Files\\MySQL\\MySQL Connector C++ 9.2\\lib64\\vs14")
 
 
 
 set(Npcap_INCLUDE_DIR "C:\\Program Files\\Npcap\\npcap-sdk-1.13\\Include")
 set(Npcap_LIB_DIR "C:\\Program Files\\Npcap\\npcap-sdk-1.13\\Lib\\x64")
+
+
+set(OpenSSL_INCLUDE_DIR "C:\\Program Files\\OpenSSL-Win64\\include")
