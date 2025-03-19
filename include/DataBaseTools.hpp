e0f29760c0b2259d46aa1461c30309792fbdfde8
UCMHCteacher <100867395+UCMHCteacher@users.noreply.github.com>
2025-03-19 22:19:02 +0800

    docs: docs of Query Strings are moved bcz it is not functioning before

--- include/DataBaseTools.hpp
+++ include/DataBaseTools.hpp
@@ -5,7 +5,8 @@
 
 #include<jdbc/mysql_driver.h>
 #include<jdbc/mysql_connection.h>
-#include<jdbc/cppconn/statement.h>
+#include<jdbc/cppconn/prepared_statement.h>
+#include<jdbc/cppconn/resultset.h>
 
 
 
@@ -23,11 +24,43 @@ namespace DataBase {
 
 
     namespace QueryStrings {
+        /**
+         * @return file_id: uint32_t
+         * @param file_name(1): varchar(255)
+         * @param file_length(2): uint64_t
+         */
         extern std::string getFileID;
+
+        /**
+         * @param file_name(1): varchar(255)
+         * @param file_length(2): uint64_t
+         */
         extern std::string insertFileInfo;
 
+
+
+
+        /**
+         * @param chunk_id(1): uint32_t
+         * @param file_id(1): uint32_t
+         * @param chunk_hash(1): binary(32)
+         * @param chunk_length(2): uint64_t
+         */
         extern std::string insertChunkInfo;
+
+        /**
+         * @return [chunk_id:uint32_t]
+         * @return [file_id:uint32_t]
+         * @param chunk_hash(1): binary(32)
+         * @param chunk_length(2): uint64_t
+         */
         extern std::string getChunkInfoByHashNLength;
+
+        /**
+         * @return [file_name:varchar(255)]
+         * @return [file_length:uint64_t]
+         * @param file_id(1): uint32_t
+         */
         extern std::string getFileInfoByFileID;
         // extern std::string ;
     }
@@ -36,3 +69,4 @@ namespace DataBase {
 
     extern sql::Connection* getConnection();
 } // namespace DataBase
+namespace DB = DataBase;
--- src/main/DataBaseTools/QueryStrings.cpp
+++ src/main/DataBaseTools/QueryStrings.cpp
@@ -4,27 +4,19 @@
 
 
 
-using namespace DataBase::QueryStrings;
 
 
 
-
-/**
- * @return file_id: uint32_t
- * @param file_name(1): varchar(255)
- * @param file_length(2): uint64_t
- */
-std::string getFileID = 
+std::string 
+DB::QueryStr::getFileID = 
     "SELECT file_id"
     "FROM files"
     "WHERE (file_name, file_length) = (?, ?)";
 
 
-/**
- * @param file_name(1): varchar(255)
- * @param file_length(2): uint64_t
- */
-std::string insertFileInfo = 
+
+std::string 
+DB::QueryStr::insertFileInfo = 
     "INSERT INTO files"
     "(file_name, file_length)"
     "VALUES"
@@ -34,37 +26,26 @@ std::string insertFileInfo =
 
 
 
-/**
- * @param chunk_id(1): uint32_t
- * @param file_id(1): uint32_t
- * @param chunk_hash(1): binary(32)
- * @param chunk_length(2): uint64_t
- */
-std::string insertChunkInfo = 
+
+std::string 
+DB::QueryStr::insertChunkInfo = 
     "INSERT INTO chunks"
     "(chunk_id, file_id, chunk_hash, chunk_length)"
     "VALUES"
     "(?, ?, ?, ?)";
 
 
-/**
- * @return [chunk_id:uint32_t]
- * @return [file_id:uint32_t]
- * @param chunk_hash(1): binary(32)
- * @param chunk_length(2): uint64_t
- */
-std::string getChunkInfoByHashNLength = 
+
+std::string 
+DB::QueryStr::getChunkInfoByHashNLength = 
     "SELECT chunk_id, file_id"
     "FROM chunks"
     "WHERE (chunk_hash, chunk_length) = (?, ?)";
 
 
-/**
- * @return [file_name:varchar(255)]
- * @return [file_length:uint64_t]
- * @param file_id(1): uint32_t
- */
-std::string getFileInfoByFileID = 
+
+std::string 
+DB::QueryStr::getFileInfoByFileID = 
     "SELECT file_name, file_length"
     "FROM files"
     "WHERE file_id = ?";
