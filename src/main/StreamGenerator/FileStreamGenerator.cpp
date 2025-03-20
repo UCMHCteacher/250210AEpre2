038b94a9efdd63da21a929aea2c5f2e0afda3ad9
UCMHCteacher <100867395+UCMHCteacher@users.noreply.github.com>
2025-03-20 13:24:47 +0800

    feat: insert file into db when it's new recorded

--- src/main/StreamGenerator/FileStreamGenerator.cpp
+++ src/main/StreamGenerator/FileStreamGenerator.cpp
@@ -1,5 +1,6 @@
 #include"StreamGenerator.hpp"
-
+#include"ToolChain.hpp"
+#include"DataBaseTools.hpp"
 
 
 
@@ -21,6 +22,52 @@ StreamGenerators::FileStreamGenerator::getStream() {
     if (_used) return nullptr;
 
     std::shared_ptr<std::istream> stream = std::make_shared<std::ifstream>(_fileName, std::ios::in | std::ios::binary);
+
+    /** Query Order: 
+     * 1) check whether the file exist or not
+     * 2) insert fileinfo (if file not exist)
+     * 3) get file_id
+    */
+    if (ToolChain::Builder::chunkProcessorActionMode & CDChunking::MainChunkProcessor::ActionMode::RecordToDataBase) {
+        uint64_t fileLength = stream->rdbuf()->pubseekoff(0, std::ios::end, std::ios::in);
+        stream->rdbuf()->pubseekpos(std::ios::beg, std::ios::in);
+
+        sql::Connection *  con = DB::getConnection();
+
+        // 1)
+        sql::PreparedStatement *  getFileIDStmt = con->prepareStatement(DB::QueryStr::getFileIDByNameNLength);
+        sql::ResultSet *  getFileIDRes = nullptr;
+        getFileIDStmt->setString(1, _fileName);
+        getFileIDStmt->setUInt64(2, fileLength);
+
+        getFileIDRes = getFileIDStmt->executeQuery();
+        if (getFileIDRes->rowsCount() > 0) {
+            std::lock_guard lk(ToolChain::_coutMutex);
+            std::cout << "File already exists in DB!\n";
+            std::exit(0);
+        }
+
+
+        // 2)
+        sql::PreparedStatement *  insertFileInfoStmt = con->prepareStatement(DB::QueryStr::insertFileInfo);
+        insertFileInfoStmt->setString(1, _fileName);
+        insertFileInfoStmt->setUInt64(2, fileLength);
+
+        insertFileInfoStmt->executeUpdate();
+
+
+        // 3)
+        getFileIDRes = getFileIDStmt->executeQuery();
+        while (getFileIDRes->next()) {
+            ToolChain::fileID_DB = getFileIDRes->getUInt("file_id");
+            std::lock_guard lk(ToolChain::_coutMutex);
+            std::cout << "file_id = " << ToolChain::fileID_DB << '\n';
+        }
+
+
+
+    }
+
     _used = true;
     return stream ? stream : nullptr;
 }
